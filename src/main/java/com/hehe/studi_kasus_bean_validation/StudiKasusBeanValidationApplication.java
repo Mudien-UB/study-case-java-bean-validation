f5e2ee392a7e6abcd7d6aebc4a26f7db027be9bc
Mudien-UB <mudienub@gmail.com>
2025-09-08 21:16:13 +0700

    f

---
+++ src/main/java/com/hehe/studi_kasus_bean_validation/StudiKasusBeanValidationApplication.java
@@ -0,0 +1,13 @@
+package com.hehe.studi_kasus_bean_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiKasusBeanValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiKasusBeanValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hehe/studi_kasus_bean_validation/StudiKasusBeanValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hehe.studi_kasus_bean_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiKasusBeanValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
